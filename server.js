386ff7ea7e7b6371ea40007103641e02ec3ef43c
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-11-24 12:03:37 +0100

    add "dev" in package .json

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { log } = require('console')
 const express = require('express')
 
 const app = express()
@@ -5,3 +6,8 @@ const app = express()
 const PORT = 3000
 
 app.use(express.static('public'))
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+
+})
